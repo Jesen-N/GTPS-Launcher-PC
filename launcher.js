2233b673de06127c113e0e0af0858622b4f19052
Jesen N <72697107+Jesen-N@users.noreply.github.com>
2021-07-31 17:45:25 +0700

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -1,40 +1,86 @@
-const tempat = "C:\\Windows\\System32\\drivers\\etc\\hosts";
-
-const fs = require('fs');
+const place = "C:\\Windows\\System32\\drivers\\etc\\hosts";
+const fs = require("fs");
 const rl = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
-console.clear()
-console.log("GTPS Launcher Made By Jesen N\n")
-rl.question("Put IP: ", IP => {
-    let tolol = `${IP} growtopia1.com\n${IP} growtopia2.com`
-    let banget = ''
+//setup here
+var ip = "ip gtps";
+let nameserver = "your name gtps";
 
-rl.question("Press Enter To Continue: ", function(number) {
-    if (number === "")
-    {
-        console.clear()
-        fs.writeFile(tempat, tolol, function(err) {
-            if (err) throw err;
-        })
-    }
+let on = `
+# Copyright (c) 1993-2009 Microsoft Corp.
+#
+# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.
+#
+# This file contains the mappings of IP addresses to host names. Each
+# entry should be kept on an individual line. The IP address should
+# be placed in the first column followed by the corresponding host name.
+# The IP address and the host name should be separated by at least one
+# space.
+#
+# Additionally, comments (such as these) may be inserted on individual
+# lines or following the machine name denoted by a '#' symbol.
+#
+# For example:
+#
+#      102.54.94.97     rhino.acme.com          # source server
+#       38.25.63.10     x.acme.com              # x client host
 
-    rl.question("GTPS Launcher ON!\n\nPress Enter Again To Turn OFF: ", function(numbers) {
-    if (numbers === "")
-    {
-        console.clear()
-        fs.writeFile(tempat, banget, function (err) {
-            if (err) throw err;
-            console.log("GTPS Launcher OFF")
+# localhost name resolution is handled within DNS itself.
+#	127.0.0.1       localhost
+#	::1             localhost
+
+${ip} growtopia1.com
+${ip} growtopia2.com
+`;
+
+let off = `
+# Copyright (c) 1993-2009 Microsoft Corp.
+#
+# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.
+#
+# This file contains the mappings of IP addresses to host names. Each
+# entry should be kept on an individual line. The IP address should
+# be placed in the first column followed by the corresponding host name.
+# The IP address and the host name should be separated by at least one
+# space.
+#
+# Additionally, comments (such as these) may be inserted on individual
+# lines or following the machine name denoted by a '#' symbol.
+#
+# For example:
+#
+#      102.54.94.97     rhino.acme.com          # source server
+#       38.25.63.10     x.acme.com              # x client host
+
+# localhost name resolution is handled within DNS itself.
+#	127.0.0.1       localhost
+#	::1             localhost
+`;
+
+console.clear();
+fs.writeFile(place, on, function (err) {
+  if (err) {
+    console.log(
+      "ERROR: Please open the application/command prompt with administrator!!"
+    );
+  } else {
+    console.log(`${nameserver} Launcher Is ON!`);
+    rl.question("Press enter to turn off: ", function (enter) {
+      if (enter === "") {
+        console.clear();
+        fs.writeFile(place, off, function (err) {
+          if (err) {
+            console.log(
+              "ERROR: Please open the application/command prompt with administrator!!"
+            );
+          } else {
+            console.log(`Now ${nameserver} Launcher Is OFF`);
+          }
         });
-    }
-    else
-    {
-        console.log("")
-        console.log("wrong number!")
-    }
-});
-});
+      }
+    });
+  }
 });
